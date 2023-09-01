be289b7bf16338428de0b8717e636da512218136
tyshbyr <tyshbyr@gmail.com>
2023-09-02 04:00:46 +0500

    Fix style

--- enricher/configs/settings.py
+++ enricher/configs/settings.py
@@ -6,7 +6,9 @@ class AppSettings(BaseSettings):
     sending_queue: str = "queue"
     rabbit_url: str = "url"
 
-    model_config = SettingsConfigDict(env_file='enricher/.env', env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(
+        env_file="enricher/.env", env_file_encoding="utf-8"
+    )
 
 
 config = AppSettings()
